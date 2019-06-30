d231e6c51355c0e9827f1f6492dc46f184724fb2
sedaji <ronwong1688@gmail.com>
2019-06-30 12:42:49 -0500

    In the course of 8 hours, went from barely knowing about mandelbrot to coding it in python

---
+++ mandelbrot.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python3
+from PIL import Image, ImageDraw
+
+z = [None] * 80
+def mandelbrot(c):
+	for n in range(80):
+		if n > 0:
+			z[n] = z[n-1]**2 + c
+			if abs(z[n]) > 2:
+				# print(f"{c}:false")
+				return False
+		else:
+			z[n] = n
+	# print(f"{c}:true")
+	return True
+
+# Image size (pixels)
+width = 600 
+height = 400 
+
+realNeg = -2
+realPos = 1
+imagiNeg = -1
+imagiPos = 1
+
+realIncrement = (realPos - realNeg) / width
+imagiIncrement = (imagiPos - imagiNeg) / height
+
+realPlots = [None] * width
+imagiPlots = [None] * height
+
+for i in range(width):
+	realNeg += realIncrement
+	realPlots[i] = realNeg
+for i in range(height):
+	imagiNeg += imagiIncrement
+	imagiPlots[i] = imagiNeg
+
+im = Image.new('RGB', (width, height), (0, 0, 0))
+draw = ImageDraw.Draw(im)
+
+color = 0
+for i in range(width):
+	for j in range(height):
+		c = complex(realPlots[i],imagiPlots[j])
+		if mandelbrot(c) == True:
+			color = 255
+			draw.point([i,j]), (color, color, color)
+			
+im.save('output.png', 'PNG')
